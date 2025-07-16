b953575f0b58f2b4bf79b91135945c337b4100f0
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2025-07-16 22:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (5.25%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -4,12 +4,12 @@ class Solution {
             return 0;
         }
         int mid=(int)Math.pow(2,n-1)/2;
+        
         if(k<=mid){
             return kthGrammar(n-1,k);
         }
         else {
             return 1-kthGrammar(n-1,k-mid);
         }
-        // return -1;
     }
 }
