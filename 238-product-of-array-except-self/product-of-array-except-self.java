e36e35dc8af9865b44200f6e90de83dbc8f378e5
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2025-11-05 15:07:30 +0530

    Time: 2 ms (88.09%) | Memory: 71.9 MB (5.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,39 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] arr = new int[n];
-        int ans = 1;
-        int zeroCount = 0;
-        int zeroIndex = -1;
-        
-        // Count zeros and calculate product of non-zero elements
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                zeroCount++;
-                zeroIndex = i;
-            } else {
-                ans = ans * nums[i];
-            }
+        int n=nums.length;
+        int ans[]=new int[n];
+        ans[0]=1;
+        for(int i=1;i<n;++i){
+            ans[i]=ans[i-1]*nums[i-1];
         }
-        
-        // Fill result array
-        for (int i = 0; i < n; i++) {
-            if (zeroCount > 1) {
-                // If more than one zero, all products are 0
-                arr[i] = 0;
-            } else if (zeroCount == 1) {
-                // If exactly one zero, only that position gets the product
-                if (i == zeroIndex) {
-                    arr[i] = ans;
-                } else {
-                    arr[i] = 0;
-                }
-            } else {
-                // No zeros, divide normally
-                arr[i] = ans / nums[i];
-            }
+        int rightP=1;
+        for(int i=n-1;i>=0;--i){
+            ans[i]*=rightP;
+            rightP*=nums[i];
         }
+        return ans;
         
-        return arr;
     }
 }
