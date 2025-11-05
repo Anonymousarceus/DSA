6573fd5078936c1489c5f575b6167a8dda857dda
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2025-11-05 19:26:10 +0530

    Time: 16 ms (21.27%) | Memory: 46.5 MB (5.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char arr1[]=s.toCharArray();
-        char arr2[]=t.toCharArray();
         if(s.length()!=t.length()){
             return false;
         }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for(int i=0;i<s.length();++i){
-            if(arr1[i]!=arr2[i]){
+        HashMap<Character,Integer>hm1=new HashMap<>();
+        // HashMap<Character,Integer>hm2=new HashMap<>();
+        for(char c:s.toCharArray()){
+            hm1.put(c,hm1.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            if(!hm1.containsKey(c)){
                 return false;
             }
+            if(hm1.get(c)>1){
+                hm1.put(c,hm1.get(c)-1);
+                
+            }
+            else{
+                hm1.remove(c);
+            }
+
+            
         }
-        return true;
+        return hm1.isEmpty();
     }
 }
