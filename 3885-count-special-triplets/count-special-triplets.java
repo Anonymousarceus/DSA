d91cfb7849df74999943fc05797095f701689730
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2025-12-09 14:58:41 +0530

    Time: 212 ms (68.22%) | Memory: 183.9 MB (48.84%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.java
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -1,23 +1,44 @@
 class Solution {
     public int specialTriplets(int[] nums) {
         final int MOD=1000000007;
-        int maxV=100000;
-        long ans=0;
-        int right[]=new int[maxV+1];
-        int left[]=new int[maxV+1];
-        for(int num:nums){
-            right[num]++;
+        Map<Integer,Integer>hm1=new HashMap<>();
+        Map<Integer,Integer>hm2=new HashMap<>();
+        for(int v:nums){
+            hm1.put(v,hm1.getOrDefault(v,0)+1);
         }
-        for(int j=0;j<nums.length;++j){
-            int mid=nums[j];
-            long tar=(long) mid*2;
-            right[mid]--;
-            if(tar<=maxV){
-                ans=(ans+((long)left[(int)tar]*right[(int)tar])%MOD)%MOD;
-            }
-            left[mid]++;
+        long ans=0;
+        for(int v:nums){
+            int tar=v*2;
+            int lCnt=hm2.getOrDefault(tar,0);
+            hm2.put(v,hm2.getOrDefault(v,0)+1);
+            int rCnt=hm1.getOrDefault(tar,0)-hm2.getOrDefault(tar,0);
+            ans=(ans+(long)lCnt*rCnt)%MOD;
         }
         return (int)ans;
-       
     }
 }
+
+
+// class Solution {
+//     public int specialTriplets(int[] nums) {
+//         final int MOD=1000000007;
+//         int maxV=100000;
+//         long ans=0;
+//         int right[]=new int[maxV+1];
+//         int left[]=new int[maxV+1];
+//         for(int num:nums){
+//             right[num]++;
+//         }
+//         for(int j=0;j<nums.length;++j){
+//             int mid=nums[j];
+//             long tar=(long) mid*2;
+//             right[mid]--;
+//             if(tar<=maxV){
+//                 ans=(ans+((long)left[(int)tar]*right[(int)tar])%MOD)%MOD;
+//             }
+//             left[mid]++;
+//         }
+//         return (int)ans;
+       
+//     }
+// }
