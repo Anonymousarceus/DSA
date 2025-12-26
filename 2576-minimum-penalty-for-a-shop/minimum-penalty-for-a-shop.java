bc6f03c9c5a2f9ead460de14b64accc37f0b3281
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2025-12-26 16:15:51 +0530

    Time: 14 ms (32.41%) | Memory: 47.1 MB (20.69%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,16 +1,49 @@
 class Solution {
     public int bestClosingTime(String customers) {
         int n=customers.length();
-        int pen=0;
-        int minP=pen,minI=0;
+        int pref[]=new int[n+1];
         for(int i=1;i<=n;++i){
-            int y=customers.charAt(i-1)=='Y'?1:-1;
-            pen-=y;
-            if(minP>pen){
-                minP=pen;
-                minI=i;
+            pref[i]=pref[i-1];
+            if(customers.charAt(i-1)=='N'){
+                pref[i]++;
             }
         }
-        return minI;
+        int suff[]=new int[n+1];
+        for(int i=n-1;i>=0;--i){
+            suff[i]=suff[i+1];
+            if(customers.charAt(i)=='Y'){
+                suff[i]++;
+            }
+        }
+        int minP=Integer.MAX_VALUE,best=0;
+        for(int j=0;j<=n;++j){
+            int p=pref[j]+suff[j];
+            if(minP>p){
+                minP=p;
+                best=j;
+            }
+        }
+        return best;
     }
 }
+
+
+
+
+// class Solution {
+//     public int bestClosingTime(String customers) {
+//         int n=customers.length();
+//         int pen=0;
+//         int minP=pen,minI=0;
+//         for(int i=1;i<=n;++i){
+//             int y=customers.charAt(i-1)=='Y'?1:-1;
+//             pen-=y;
+//             if(minP>pen){
+//                 minP=pen;
+//                 minI=i;
+//             }
+//         }
+//         return minI;
+//     }
+    
+// }
