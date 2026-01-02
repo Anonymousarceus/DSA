d110b0b38cab4b6520ab700dc9afe715e21ca707
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2026-01-02 16:20:26 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (36.21%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,16 +1,25 @@
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
+//         int n=nums.length;
+//         int x=n/2;
+//         HashMap<Integer,Integer>hm=new HashMap<>();
+//         for(int i:nums){
+//             hm.put(i,hm.getOrDefault(i,0)+1);
+//         }
+//         for(Map.Entry<Integer,Integer>h:hm.entrySet()){
+//             if(h.getValue()==x){
+//                 return h.getKey();
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
-    public int repeatedNTimes(int[] nums) {
-        int n=nums.length;
-        int x=n/2;
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i:nums){
-            hm.put(i,hm.getOrDefault(i,0)+1);
-        }
-        for(Map.Entry<Integer,Integer>h:hm.entrySet()){
-            if(h.getValue()==x){
-                return h.getKey();
-            }
-        }
-        return -1;
+    public int repeatedNTimes(int[] A) {
+        for (int i = 0; i < A.length - 2; i++)
+            if (A[i] == A[i + 1] || A[i] == A[i + 2])
+                return A[i];
+        return A[A.length - 1];
     }
 }
