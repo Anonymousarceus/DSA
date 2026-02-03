49c7115116de64e8549f861e7d4747d34f60c490
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2026-02-03 15:44:07 +0530

    Time: 1 ms (72.27%) | Memory: 44.6 MB (64.71%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,22 +1,36 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int n=nums.length;
-        int i=0;
-        while(i<n-1 && nums[i]<nums[i+1]){
-            ++i;
+        int n = nums.length;
+        int p = 0, q = 0, x = 0;
+        
+        // Find end of first increasing phase
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums[i] < nums[i + 1]) {
+                ++p;
+            } else {
+                break;
+            }
         }
-        if(i==0 || i==n-1){
-            return false;
+        
+        // Find end of decreasing phase
+        for (int i = p; i < n - 1; ++i) {
+            if (nums[i] > nums[i + 1]) {
+                ++q;
+            } else {
+                break;
+            }
         }
-        while(i<n-1 && nums[i]>nums[i+1]){
-            ++i;
+        
+        // Find end of second increasing phase
+        for (int i = p + q; i < n - 1; ++i) {
+            if (nums[i] < nums[i + 1]) {
+                ++x;
+            } else {
+                break;
+            }
         }
-        if(i==n-1){
-            return false;
-        }
-        while(i<n-1 && nums[i]<nums[i+1]){
-            ++i;
-        }
-        return i==n-1;
+        
+        // Check if all three phases exist and cover entire array
+        return p > 0 && q > 0 && x > 0 && p + q + x == n - 1;
     }
 }
