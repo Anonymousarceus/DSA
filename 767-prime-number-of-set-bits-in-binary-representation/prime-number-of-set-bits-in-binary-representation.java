2b3ee766806d57d62169690d3b308df288920597
Akshat Mishra <108983983+Anonymousarceus@users.noreply.github.com>
2026-02-21 10:40:37 +0530

    Time: 6 ms (54.33%) | Memory: 42.6 MB (27.37%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,33 +1,13 @@
 class Solution {
-    public int countPrimeSetBits(int left, int right) {
-        int count=0;
-        for(int i=left;i<=right;++i){
-            String x=Integer.toBinaryString(i);
-            int a=0;
-            for(char c:x.toCharArray()){
-                
-                if(c=='1'){
-                    ++a;
-                }
-            }
-            if(isPrime(a)){
-                ++count;
-            }
-            
-
-        }
-        return count;
+    public int countPrimeSetBits(int L, int R) {
+        int ans = 0;
+        for (int x = L; x <= R; ++x)
+            if (isSmallPrime(Integer.bitCount(x)))
+                ans++;
+        return ans;
     }
-    public boolean isPrime(int n){
-        if(n==0||n==1){
-            return false;
-        }
-        for(int i=2;i<=n/2;++i){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
-        
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
     }
 }
